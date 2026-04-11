5ad62818640d6b6522a64bab8319fd080d08d392
EduTSmart <edutsmart@gmail.com>
2026-04-11 14:20:49 +0700

    lab8 add chat

---
+++ src/main/java/com/fpoly/config/WebSocketConfig.java
@@ -0,0 +1,26 @@
+package com.fpoly.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        // Endpoint để client kết nối vào
+        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        // Kênh để client nhận tin nhắn (Subscribe)
+        registry.enableSimpleBroker("/topic");
+        // Tiền tố cho các request từ client gửi lên (Send)
+        registry.setApplicationDestinationPrefixes("/app");
+    }
+}
